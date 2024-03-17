0f7c22a330ba7e0adf75ee843476bd833eae1e31
João Souza <ms.joao.felipe@gmail.com>
2024-03-17 17:36:48 -0300

    feat: Add remote backend

--- provider.tf
+++ provider.tf
@@ -1,10 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.41.0"
     }
   }
+  backend "s3" {
+    bucket = "sredevops-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
